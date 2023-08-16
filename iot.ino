07da99986af4d11a0030a21b912730e0f3eddbe3
taufikrezza85 <66150862+taufikrezza85@users.noreply.github.com>
2023-08-16 15:13:46 +0800

    Update iot.ino

--- iot.ino
+++ iot.ino
@@ -1,11 +1,7 @@
 #include <Adafruit_NeoPixel.h>
- Adafruit_NeoPixel pixels = Adafruit_NeoPixel(5, 4, NEO_GRB + NEO_KHZ800);
 
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(5, 4, NEO_GRB + NEO_KHZ800);
 
-
-int value1;
-int value2;
-int value3;
 //declare buzer pin
 const int buzer=2;
 
@@ -13,18 +9,21 @@ void setup() {
   pixels.begin();
 
   Serial.begin(9600);
-  Blynk.begin(auth, ssid, pass);
+  
   //CODING BY MOHAMAD NOH
   pinMode(34, INPUT);
   pinMode(36, INPUT);
+	
   pinMode(buzer,OUTPUT); //set pinMode for buzer
-  Timer1.setInterval(300, Timer1_TimerEvent);
+	
+ 
 
 }
 
 void loop() {
    int ultra = ultrasonic(12,27);
   int light1 = analogRead(36);
+	
   //CODING ADD BY MOHAMAD NOH
   int analogpin34 = analogRead(34);
 	
