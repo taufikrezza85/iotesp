becfe9cafeb610f991275719b640afce96f85af3
taufikrezza85 <66150862+taufikrezza85@users.noreply.github.com>
2023-08-16 15:34:39 +0800

    Update iot.ino

--- iot/iot.ino
+++ iot/iot.ino
@@ -1,1 +1,15 @@
+void setup(){
+  Serial.begin(9600);
+  pinMode(22,OUTPUT);
+  pinMode(34,INPUT);
+}
 
+void loop(){
+
+  float value1 = analogRead(34);
+
+  Serial.print ("Value:");
+  Serial.println(value1);
+  
+  
+}
